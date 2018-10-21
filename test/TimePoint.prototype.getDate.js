cf4ed42f77de05262dcfd47649bda5771a7d7239
John Yuan <jyuanco@outlook.com>
2018-10-21 19:44:08 +0800

    添加 TimePoint.prototype.getDate 单元测试

---
+++ test/TimePoint.prototype.getDate.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const TimePoint = require('../dist/cmd/TimePoint')
+
+describe(`TimePoint.prototype.getDate`, () => {
+
+    it(`getDate should return an instance of Date`, () => {
+        const t = TimePoint.from()
+        const d = t.getDate()
+
+        assert.equal(d instanceof Date, true)
+    })
+
+    it(`getDate should return new instance of Date each time`, () => {
+        const t = TimePoint.from()
+        const d1 = t.getDate()
+        const d2 = t.getDate()
+
+        assert.equal(d1 === d2, false)
+    })
+
+    it(`the date should have the same time with the TimePoint`, () => {
+        const t = TimePoint.from();
+        const d = t.getDate()
+
+        assert.equal(t.getTime(), d.getTime())
+    })
+
+})
